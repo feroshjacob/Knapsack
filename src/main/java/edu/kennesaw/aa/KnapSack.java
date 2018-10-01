6a0208fe11fb8f019315952cee02bb99c77d4fb6
feroshjacob <ferosh_jacob@homepot.com>
2018-10-01 06:51:51 -0400

    with solution

--- src/main/java/edu/kennesaw/aa/KnapSack.java
+++ src/main/java/edu/kennesaw/aa/KnapSack.java
@@ -5,28 +5,88 @@ import java.util.List;
 
 public class KnapSack {
 
-    private List<ValueAndWeight> valueAndWeights=null;
+    private List<ValueAndWeight> valueAndWeights;
+    private int[][] table;
+    private List<ValueAndWeight> selectedOnes;
 
     /*
      Complete these methods. You are free to add more variable as required in the class. But keep the method signature as it is.
      */
     private int maxWeight =0;
+    private int maxValue=0;
     public KnapSack(List<ValueAndWeight> valueAndWeights, int maxWeight ) {
 
         this.valueAndWeights = valueAndWeights;
         this.maxWeight = maxWeight;
+        executeKnapsack();
+    }
+    public void executeKnapsack() {
+        //checking preconditions
+        assert (valueAndWeights.size() > 0);
+        for(ValueAndWeight v: valueAndWeights){
+            assert (v.getValue() >0);
+            assert (v.getWeight() >0);
+        }
+
+
+        int  numItems = valueAndWeights.size();
+        int capacity = maxWeight;
+        table = new int[numItems + 1][capacity + 1];
+        boolean[][] knapsack = new boolean[numItems + 1][capacity + 1];
+
+        //Build table k[][] in bottom up manner
+        for (int i = 1; i <= numItems; i++) {
+            for (int w = 1; w <= capacity; w++) {
+                //don't take the item
+                int f1 = table[i - 1][w];
+
+                //take it
+                int f2 = Integer.MIN_VALUE;
+                if (i < numItems && valueAndWeights.get(i).getValue() <= w) {
+                    f2 = valueAndWeights.get(i).getWeight() + table[i - 1][w - valueAndWeights.get(i).getValue()];
+                }
+                //select the better of two options
+                table[i][w] = Math.max(f1, f2);
+                knapsack[i][w] = (f2 > f1);
+
+            }
+        }
+
+
+        //determine which items to take
+        boolean[] take = new boolean[numItems + 1];
+        selectedOnes = new ArrayList<ValueAndWeight>();
+        for (int n = numItems, w = maxWeight; n > 0; n--) {
+            if (knapsack[n][w]) {
+                take[n] = true;
+                selectedOnes.add(valueAndWeights.get(n));
+                w = w- valueAndWeights.get(n).getValue();
+            } else
+                take[n] = false;
+        }
+
+
+        for(ValueAndWeight each:selectedOnes){
+            maxValue = maxValue +each.getWeight();
+        }
+
+
+
     }
 
+
+
+
     public List<ValueAndWeight> getOptimalItems() {
-        return new ArrayList<ValueAndWeight>();
+        return selectedOnes;
     }
 
     public Integer getMaximumValue(){
-        return 90;
+        return maxValue;
     }
 
     //Order is (i,w)
     public int[][] getTable() {
-        return new int[0][0];
+        return table;
     }
 }
--- src/test/java/edu/kennesaw/aa/KnapSackTest.java
+++ src/test/java/edu/kennesaw/aa/KnapSackTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class KnapSackTest  {
 
@@ -34,8 +35,50 @@ public class KnapSackTest  {
         Assert.assertEquals(table[4][10], 90);
         Assert.assertEquals(table[4][3], 50);
 
+    }
 
+    @Test
+    public void testRandomKnapSacks(){
 
+        Random generator = new Random();
+        List<ValueAndWeight> valueAndWeights = new ArrayList<ValueAndWeight>();
+
+        for(int i =0;i < 20; i++) {
+            valueAndWeights.add(new ValueAndWeight(generator.nextInt(100)+1, generator.nextInt(100)+1));
+        }
+        KnapSack knapSack = new KnapSack(valueAndWeights,10);
+        Assert.assertTrue(knapSack.getMaximumValue()>1);
+        List<ValueAndWeight> solution = knapSack.getOptimalItems();
+        Assert.assertTrue(solution.size()>0);
+
+        int[][] table = knapSack.getTable();
+        Assert.assertTrue(table.length>0);
 
     }
+
+
+    @Test
+    public void testKnapSack2(){
+
+        List<ValueAndWeight> valueAndWeights = new ArrayList<ValueAndWeight>();
+
+        for(int i =0;i < 20; i++) {
+            valueAndWeights.add(new ValueAndWeight(i+1, 20-i));
+        }
+        KnapSack knapSack = new KnapSack(valueAndWeights,10);
+
+        Assert.assertEquals(knapSack.getMaximumValue().intValue(),74);
+        List<ValueAndWeight> solution = knapSack.getOptimalItems();
+        Assert.assertTrue(solution.size()==4);
+        Assert.assertTrue(solution.contains(new ValueAndWeight(20,1)));
+        Assert.assertTrue(solution.contains(new ValueAndWeight(19,2)));
+        Assert.assertTrue(solution.contains(new ValueAndWeight(18,3)));
+        Assert.assertTrue(solution.contains(new ValueAndWeight(17,4)));
+
+        int[][] table = knapSack.getTable();
+        Assert.assertTrue(table.length>0);
+
+    }
+
+
 }
